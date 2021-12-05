39c422a64f3f7dbbf359b18df8541e21fcf43a8d
cocotran <anhquangtran2703@gmail.com>
2021-12-04 23:09:14 -0500

    Reformat code

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,27 +3,31 @@ use std::fs;
 
 struct Cell {
     number: String,
-    hit: bool
+    hit: bool,
 }
 
 impl Cell {
     fn hit_cell(&mut self) {
         self.hit = true;
-    } 
+    }
 }
 
 struct Board {
     cells: Vec<Cell>,
-    win: bool
+    win: bool,
 }
 
 impl Board {
     const WIDTH: usize = 5;
 
     fn tick(&mut self, cell_num: &str) {
-        if let Some(c) = self.cells.iter_mut().find(|cell| cell.number == cell_num.to_string()) {
+        if let Some(c) = self
+            .cells
+            .iter_mut()
+            .find(|cell| cell.number == cell_num.to_string())
+        {
             c.hit_cell();
-        } 
+        }
     }
 
     fn mark_win(&mut self) {
@@ -40,7 +44,13 @@ impl Board {
 
         // Check columns
         for column in 0..Self::WIDTH {
-            if self.cells.iter().skip(column).step_by(Self::WIDTH).all(|cell| cell.hit) {
+            if self
+                .cells
+                .iter()
+                .skip(column)
+                .step_by(Self::WIDTH)
+                .all(|cell| cell.hit)
+            {
                 return true;
             }
         }
@@ -50,12 +60,15 @@ impl Board {
 
     fn get_score(&self, num: i64) -> i64 {
         let mut score: i64 = 0;
-        self.cells.iter().filter(|cell| !cell.hit).for_each(|cell| {let s: i64 = cell.number.parse().unwrap(); score += s});
+        self.cells.iter().filter(|cell| !cell.hit).for_each(|cell| {
+            let s: i64 = cell.number.parse().unwrap();
+            score += s
+        });
         return score * num;
     }
 }
 
-fn build_new_board(board: &str) -> Board{
+fn build_new_board(board: &str) -> Board {
     let mut cells: Vec<Cell> = vec![];
 
     let board_rows: Vec<&str> = board.split("\n").collect();
@@ -63,12 +76,18 @@ fn build_new_board(board: &str) -> Board{
         let row_cells: Vec<&str> = row.split(" ").collect();
         for cell in row_cells {
             if cell != "" {
-                cells.push(Cell { number: String::from(cell), hit: false});
+                cells.push(Cell {
+                    number: String::from(cell),
+                    hit: false,
+                });
             }
         }
     }
 
-    return Board { cells: cells, win: false };
+    return Board {
+        cells: cells,
+        win: false,
+    };
 }
 
 fn main() {
